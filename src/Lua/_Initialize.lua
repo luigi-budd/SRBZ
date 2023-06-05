efdc03923d4a9d2024546ad63dced163c3554559
Blur <danielthesmartypants2017@gmail.com>
2023-06-04 19:30:37 -0500

    w

--- src/Lua/_Initialize.lua
+++ src/Lua/_Initialize.lua
@@ -1,15 +1,15 @@
 rawset(_G, "SRBZ", {})
 rawset(_G, "XSLINGER", {})
-
+freeslot("TOL_ZESCAPE")
 SRBZ.survivaltime = (60*10)*TICRATE
 SRBZ.swarmtime = (60*5)*TICRATE
 SRBZ.onwinscreen = false
 SRBZ.wintics = 0 -- How many tics after a win screen. Resets on mapload.
 
 G_AddGametype({
-	name = "Zombie Escape",
-	identifier = "zescape",
-	typeoflevel = TOL_ZESCAPE,
+	name = "Sonic Robo Blast Zombies",
+	identifier = "srbz",
+	typeoflevel = TOL_SRBZ,
 	rules = GTR_RINGSLINGER|GTR_TEAMS|GTR_HURTMESSAGES|GTR_TIMELIMIT|GTR_ALLOWEXIT|GTR_RESPAWNDELAY|GTR_SPAWNENEMIES|GTR_CUTSCENES|GTR_TEAMFLAGS,
 	intermissiontype = int_teammatch,
 	headerleftcolor = 152,
