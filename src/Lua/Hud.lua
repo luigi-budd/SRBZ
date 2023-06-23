1a18a17828d9e5d5bcb2c67364de318cc632e8fd
Blur <danielthesmartypants2017@gmail.com>
2023-06-23 14:58:06 -0500

    roobiesfake

--- src/Lua/Hud.lua
+++ src/Lua/Hud.lua
@@ -9,6 +9,7 @@ customhud.SetupFont("STCFC", -1, 4);
 local testhud = function(v, player)
 	if gametype ~= GT_SRBZ then return end
 	customhud.CustomFontString(v, 0, 190, player.name.." as "..skins[player.mo.skin].name, "STCFC", (V_SNAPTOBOTTOM|V_SNAPTOLEFT), nil , nil, player.skincolor) --
+	customhud.CustomFontString(v, 0, 182, "Rubies: 10", "STCFC", (V_SNAPTOBOTTOM|V_SNAPTOLEFT), nil , nil, SKINCOLOR_CRIMSON)
 end
 
 local togglehud = function(v, player)
