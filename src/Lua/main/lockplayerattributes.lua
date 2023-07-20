4efc3d793c1463f71871760855b87b820024b316
Jisk <37682565+Jiskster@users.noreply.github.com>
2023-07-20 00:22:18 -0500

    oops

--- src/Lua/main/lockplayerattributes.lua
+++ src/Lua/main/lockplayerattributes.lua
@@ -2,10 +2,10 @@ addHook("PlayerThink", function(player)
 	if gametype ~= GT_SRBZ then return end
 	
 	if player.mo and player.mo.valid then
-		if player.zteam == 2 and player.skin ~= "zzombie" then
+		if player.zteam == 2 and player.mo.skin ~= "zzombie" then
 			R_SetPlayerSkin(player, "zzombie")
 			player.mo.color = SKINCOLOR_MOSS
-		elseif player.zteam == 1 and player.skin == "zzombie" then
+		elseif player.zteam == 1 and player.mo.skin == "zzombie" then
 			R_SetPlayerSkin(player, "sonic")
 			player.mo.color = player.skincolor
 		end
