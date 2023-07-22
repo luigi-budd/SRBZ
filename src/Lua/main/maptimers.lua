0ab1c1240d0fe1c60718dcd80798e5dfcab4f363
LeonardoTheMutant <olekseilvanov435@gmail.com>
2023-07-22 17:09:57 +0300

    Fix typos in maptimers.lua

--- src/Lua/main/maptimers.lua
+++ src/Lua/main/maptimers.lua
@@ -8,10 +8,10 @@
 --
 --Access your timer:
 --  SRBZ.MapTimers["Your_Timer_Name"].time - get the current time of your timer
---  SRBZ.MapTimers["Your_Timer_Name"].state - is timer active or not, true = is it counting down, false = it is paused
+--  SRBZ.MapTimers["Your_Timer_Name"].active - is timer active or not, true = is it counting down, false = it is paused
 --
---How to actually use it on your map with Linedef Executor (LUA example):
---	addHook("LinedefExecute", function(
+--How to actually use it on your map (ThinkFrame hook LUA example):
+--	addHook("ThinkFrame", function(
 --      local mytimer=SRBZ.MapTimers["MyTimer"]
 --      if mytimer.time==105 then
 --          print("3 SECONDS LEFT!")
@@ -19,12 +19,12 @@
 --          print("2 SECONDS LEFT!!")
 --      elseif mytimer.time==35 then
 --          print("ONE SECOND!!!!")
---      elseif my.timer.time==0 then
+--      elseif my.timer.time==0 and not mytimer.active then
 --          print("TIME OVER!")
 --          dosomethingelse()
 --      end
---      if (mytimer.state==false and mytimer.time ~= 0) print("WHY THE FUCK MY TIMER IS STOPPED") end --please ignore this line :skull:
---  end), "FUNCNAME")
+--      if (not mytimer.active and mytimer.time ~= 0) print("WHY THE FUCK MY TIMER IS STOPPED") end --please ignore this line :skull:
+--  end))
 
 SRBZ.MapTimers={}
 
@@ -32,11 +32,11 @@ SRBZ.AddMapTimer=function(n,m,t,a)
 	if n==nil error("Name of the Timer is not specified") end
 	if m==nil error("Map ID is not specified") end
 	if t==nil error("Time is not specified") end
-	if type(n)!="string" error("Name of the Timer should be string") end
-	if type(m)!="number" error("Map ID is not number") end
-	if type(t)!="number" error("Time is not number") end
+	if type(n)!="string" error("Timer Name should be string") end
+	if type(m)!="number" error("Map ID should be number") end
+	if type(t)!="number" error("Time should be number in ticks") end
 	--if type(timepoint)~="table" and type(timepoint)~="nil" error("TimePoint values must be a table") end
-	if type(a)!="boolean" error ("Timer Activity value is not boolean") end
+	if type(a)!="boolean" error ("Timer Activity value should be boolean") end
 	SRBZ.MapTimers[n]={map=m,time=t,active=a,originaltime=t}
 end
 SRBZ.ToggleMapTimer=function(timer)
