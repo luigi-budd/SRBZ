d11d31643b59b5c102ab1d42f94f74717e248e77
Jisk <37682565+Jiskster@users.noreply.github.com>
2023-07-22 12:07:11 -0500

    dont give srbz stuff when not in srbz

--- src/Lua/main/giveplayerflags.lua
+++ src/Lua/main/giveplayerflags.lua
@@ -1,4 +1,5 @@
 SRBZ.giveplayerflags = function(player)
+	if gametype ~= GT_SRBZ then return end
 	player.charflags = SF_NOJUMPSPIN|SF_NOJUMPDAMAGE|SF_NOSKID
 	player.pflags = $ & ~PF_DIRECTIONCHAR
 	SRBZ.SetCCtoplayer(player)
