debda409ad558096329015a5ca7898eafffbd679
LeonardoTheMutant <olekseilvanov435@gmail.com>
2023-07-22 19:08:36 +0300

    Move NPC init values to initvalues.lua

--- src/Lua/main/initvalues.lua
+++ src/Lua/main/initvalues.lua
@@ -7,3 +7,11 @@ SRBZ.playerfunc = function(player)
 		SRBZ.SetCChealth(player)
 	end
 end
+
+mobjinfo[MT_BLUECRAWLA].npc_name = "Blue Crawla"
+mobjinfo[MT_BLUECRAWLA].min_spawnhealth = 2
+mobjinfo[MT_BLUECRAWLA].max_spawnhealth = 7
+
+mobjinfo[MT_REDCRAWLA].npc_name = "Red Crawla"
+mobjinfo[MT_REDCRAWLA].min_spawnhealth = 5
+mobjinfo[MT_REDCRAWLA].max_spawnhealth = 12
