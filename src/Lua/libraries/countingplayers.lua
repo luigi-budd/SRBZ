c3a5148f5fa27ff8c0f4a721ee3371950db831b1
LeonardoTheMutant <olekseilvanov435@gmail.com>
2023-07-22 19:22:56 +0300

    Simplify code

--- src/Lua/libraries/countingplayers.lua
+++ src/Lua/libraries/countingplayers.lua
@@ -1,34 +1,17 @@
 SRBZ.SurvivorCount = function()
-	local c = 0 
-	
-	for player in players.iterate do
-		if player.zteam ~= nil and player.zteam == 1 then
-			c = $ + 1
-		end
-	end
-	
-	return c;
+	local c = 0
+	for player in players.iterate do if player.zteam ~= nil and player.zteam == 1 then c = $ + 1 end end
+	return c
 end
 
 SRBZ.ZombieCount = function()
-	local c = 0 
-	
-	for player in players.iterate do
-		if player.zteam ~= nil and player.zteam == 2 then
-			c = $ + 1
-		end
-	end
-	
-	return c;
+	local c = 0
+	for player in players.iterate do if player.zteam ~= nil and player.zteam == 2 then c = $ + 1 end end
+	return c
 end
 
-
 SRBZ.PlayerCount = function()
-	local c = 0 
-	
-	for player in players.iterate do
-		c = $ + 1
-	end
-	
-	return c;
+	local c = 0
+	for player in players.iterate do c = $ + 1 end
+	return c
 end
