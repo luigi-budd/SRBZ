0bb38b45a8b64f020d301476587b0526c7402135
LeonardoTheMutant <70893504+LeonardoTheMutant@users.noreply.github.com>
2023-07-27 10:53:23 +0300

    Fix typo
    65536 is not equal to FRACUNIT...

--- src/Lua/main/capitalism.lua
+++ src/Lua/main/capitalism.lua
@@ -39,7 +39,7 @@ addHook("MobjDeath", function(mobj)
 			P_SetObjectMomZ(the_ruby, P_RandomRange(5,10)<<16)
 		
 			if ruby_count > 1 then
-				local angle = P_RandomByte() * 256 * FU
+				local angle = P_RandomByte() * 256 * 2^16
 				P_InstaThrust(the_ruby, angle, 3*FU)
 			end
 		end
