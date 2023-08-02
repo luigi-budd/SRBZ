c63bbe03acfad6411f22eaa821f4d7d5606bd433
Jisk <37682565+Jiskster@users.noreply.github.com>
2023-08-02 13:14:16 -0500

    delete prototype comment

--- src/Lua/hud/inventory.lua
+++ src/Lua/hud/inventory.lua
@@ -36,25 +36,5 @@ SRBZ.inventoryhud = function(v, player)
 			v.drawStretched(x, y, FU, FU, patch, V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_TRANSLUCENT)
 		end
 	end
-	/*
-	for i,value in ipairs(player["srbz_info"].inventory) do
-		if i > player["srbz_info"].inventory_limit then 
-			-- dont show more than what you need
-			continue
-		end
-		local x = 116*FU
-		local y = 176*FU
-		local overone_xpos = ((i-1)*20)*FU
-		local patch = v.cachePatch(player["srbz_info"].inventory[i].icon)
-		
-		if i > 1 then
-			x = $ + overone_xpos
-		end
-		
-		-- weapon icon
-		v.drawStretched(x, y, FU, FU, patch, V_SNAPTOLEFT|V_SNAPTOBOTTOM)
-	end
-	*/
-	-- selection
 	v.drawStretched(sel_x-(2*FU), sel_y-(2*FU), FU, FU, s_patch, V_SNAPTOLEFT|V_SNAPTOBOTTOM)
 end
