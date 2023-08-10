8d7ce5a5b250b368144a88537ee72dfaf9d148a1
Blur <danielthesmartypants2017@gmail.com>
2023-08-10 14:05:52 -0500

    Stop this.
    Putting code in one line doesn't make it faster or better. it just makes it even more of a headache for smaller screens or windows. and forces you to read left to right all the time

--- src/Lua/libraries/countingplayers.lua
+++ src/Lua/libraries/countingplayers.lua
@@ -1,17 +1,27 @@
 SRBZ.SurvivorCount = function()
 	local c = 0
-	for player in players.iterate do if player.zteam ~= nil and player.zteam == 1 then c = $ + 1 end end
+	for player in players.iterate do 
+		if player.zteam ~= nil and player.zteam == 1 and not player.spectator then 
+			c = $ + 1 
+		end 
+	end
 	return c
 end
 
 SRBZ.ZombieCount = function()
 	local c = 0
-	for player in players.iterate do if player.zteam ~= nil and player.zteam == 2 then c = $ + 1 end end
+	for player in players.iterate do 
+		if player.zteam ~= nil and player.zteam == 2 and not player.spectator then 
+			c = $ + 1 
+		end
+	end
 	return c
 end
 
 SRBZ.PlayerCount = function()
 	local c = 0
-	for player in players.iterate do c = $ + 1 end
+	for player in players.iterate do 
+		c = $ + 1 
+	end
 	return c
 end
