4d16858099b64532e2252d73be5f2f32ad832056
Jisk <37682565+Jiskster@users.noreply.github.com>
2023-08-11 14:10:33 -0500

    scatter ring

--- src/Lua/main/weapons.lua
+++ src/Lua/main/weapons.lua
@@ -19,6 +19,71 @@ freeslot(
 
 freeslot("MT_PROPWOOD","S_PROP1","S_PROP1_BREAK","SPR_WPRP")
 freeslot("MT_MIRRORCLONE")
+freeslot(
+"MT_SRBZ_THROWNSCATTER",
+"S_SRBZ_THROWNSCATTER1",
+"S_SRBZ_THROWNSCATTER2",
+"S_SRBZ_THROWNSCATTER3",
+"S_SRBZ_THROWNSCATTER4",
+"S_SRBZ_THROWNSCATTER5",
+"S_SRBZ_THROWNSCATTER6",
+"S_SRBZ_THROWNSCATTER7"
+)
+freeslot("sfx_shgn")
+-- RSNEO slap
+mobjinfo[MT_SRBZ_THROWNSCATTER] = { 
+	spawnstate = S_SRBZ_THROWNSCATTER1,
+	--activesound = sfx_shgn,
+	deathstate = S_SPRK1,
+	xdeathstate = S_SPRK1,
+	speed = 60*FRACUNIT,
+	radius = 16*FRACUNIT,
+	height = 32*FRACUNIT,
+	flags = MF_NOBLOCKMAP|MF_MISSILE|MF_NOGRAVITY
+}
+
+states[S_SRBZ_THROWNSCATTER1] = {
+	nextstate = S_SRBZ_THROWNSCATTER2,
+	sprite = SPR_TSCR,
+	frame = FF_FULLBRIGHT,
+	tics = 1,
+}
+states[S_SRBZ_THROWNSCATTER2] = {
+	nextstate = S_SRBZ_THROWNSCATTER3,
+	sprite = SPR_TSCR,
+	frame = FF_FULLBRIGHT,
+	tics = 1,
+}
+states[S_SRBZ_THROWNSCATTER3] = {
+	nextstate = S_SRBZ_THROWNSCATTER4,
+	sprite = SPR_TSCR,
+	frame = FF_FULLBRIGHT,
+	tics = 1,
+}
+states[S_SRBZ_THROWNSCATTER4] = {
+	nextstate = S_SRBZ_THROWNSCATTER5,
+	sprite = SPR_TSCR,
+	frame = FF_FULLBRIGHT,
+	tics = 1,
+}
+states[S_SRBZ_THROWNSCATTER5] = {
+	nextstate = S_SRBZ_THROWNSCATTER6,
+	sprite = SPR_TSCR,
+	frame = FF_FULLBRIGHT,
+	tics = 1,
+}
+states[S_SRBZ_THROWNSCATTER6] = {
+	nextstate = S_SRBZ_THROWNSCATTER7,
+	sprite = SPR_TSCR,
+	frame = FF_FULLBRIGHT,
+	tics = 1,
+}
+states[S_SRBZ_THROWNSCATTER7] = {
+	nextstate = S_SRBZ_THROWNSCATTER1,
+	sprite = SPR_TSCR,
+	frame = FF_FULLBRIGHT,
+	tics = 1,
+}
 
 mobjinfo[MT_MIRRORCLONE] = {
     doomednum = -1,
@@ -102,7 +167,7 @@ SRBZ:CreateItem("Red Ring",  {
 	color = SKINCOLOR_RED,
 	knockback = 45*FRACUNIT,
 	damage = 17,
-	price = 50,
+	price = 60,
 })
 
 SRBZ:CreateItem("Automatic Ring",  {
@@ -113,7 +178,7 @@ SRBZ:CreateItem("Automatic Ring",  {
 	damage = 9,
 	knockback = 30*FRACUNIT,
 	flags2 = MF2_AUTOMATIC,
-	price = 130,
+	price = 170,
 })
 
 SRBZ:CreateItem("Apple", {
@@ -228,7 +293,7 @@ SRBZ:CreateItem("Explosion Ring", {
 	color = SKINCOLOR_BLACK,
 	damage = 29,
 	knockback = 90*FRACUNIT,
-	price = 230,
+	price = 270,
 })
 
 SRBZ:CreateItem("Negative Ring",  {
@@ -238,7 +303,7 @@ SRBZ:CreateItem("Negative Ring",  {
 	color = SKINCOLOR_WHITE,
 	knockback = -45*FRACUNIT,
 	damage = 19,
-	price = 650,
+	price = 550,
 })
 
 SRBZ:CreateItem("Blue Spring",  {
@@ -265,5 +330,57 @@ SRBZ:CreateItem("Bounce Ring",  {
 	damage = 11,
 	fuse = 10*TICRATE,
 	--flags2 = MF2_BOUNCERING,
-	price = 90,
+	price = 120,
+})
+
+SRBZ:CreateItem("Scatter Ring",  {
+	icon = "SCATIND",
+	firerate = TICRATE,
+	sound = sfx_shgn,
+	knockback = 13*FRACUNIT,
+	damage = 11,
+	fuse = TICRATE/2,
+	color = SKINCOLOR_PURPLE,
+	price = 340,
+	ontrigger = function(player)
+		local mt = MT_SRBZ_THROWNSCATTER
+		local mo = player.mo
+		local spread = 4
+		--S_StartSound(mo, sfx_shgn)
+		for i = -1, 1
+			local shot = P_SPMAngle(mo, mt, mo.angle + i * ANG1*spread, 1, 0)
+			if shot and shot.valid
+				shot.color = SRBZ:FetchInventorySlot(player).color
+				shot.fuse = SRBZ:FetchInventorySlot(player).fuse
+				shot.forcedamage = SRBZ:FetchInventorySlot(player).damage
+
+				shot.momx = $ + mo.momx / 3
+				shot.momy = $ + mo.momy / 3
+				shot.momz = $ + mo.momz / 3
+			end
+		end
+		for i = -1, 1, 2
+			local prevaim = player.aiming
+			player.aiming = $ + i * ANG1*spread
+			local shot = P_SPMAngle(mo, mt, mo.angle, 1, 0)
+			player.aiming = prevaim
+			if shot and shot.valid
+				shot.color = SRBZ:FetchInventorySlot(player).color
+				shot.fuse = SRBZ:FetchInventorySlot(player).fuse
+				shot.forcedamage = SRBZ:FetchInventorySlot(player).damage
+
+				shot.momx = $ + mo.momx / 3
+				shot.momy = $ + mo.momy / 3
+				shot.momz = $ + mo.momz / 3
+			end
+		end
+		if not P_IsObjectOnGround(mo)
+			local aim = max(-FRACUNIT, min(FRACUNIT, -player.aiming/13000))
+			if P_MobjFlip(mo) * aim > 0
+				aim = $ * 2/8
+			end
+			mo.momz = $ + FixedMul(mo.scale, aim)
+			P_Thrust(mo, mo.angle, -FRACUNIT*9)
+		end
+	end
 })
