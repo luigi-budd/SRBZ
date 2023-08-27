9e0720909df24ec4b1a07c07d85e22a935368ed7
LeonardoTheMutant <olekseilvanov435@gmail.com>
2023-08-27 16:02:46 +0200

    Get rid of the MaxHealth in CharConfigs

--- src/Lua/main/characterconfigs.lua
+++ src/Lua/main/characterconfigs.lua
@@ -1,3 +1,6 @@
+-- health and maxhealth are identical values, I see no sense in declaring two of them
+-- (perhaps players starting with lower hp?)
+
 SRBZ.SetCCtoplayer = function(player)
 	local pmo = player.mo
 	local cc = SRBZ.CharacterConfig
@@ -23,14 +26,19 @@ SRBZ.SetCChealth = function(player)
 	local pmo = player.mo
 	local cc = SRBZ.CharacterConfig
 	if cc[pmo.skin] then
-		if (cc[pmo.skin].health) then pmo.health = cc[pmo.skin].health
-		else pmo.health = cc["default"].health end
+		if (cc[pmo.skin].health) then
+			pmo.health = cc[pmo.skin].health
+			pmo.maxhealth = pmo.health
+		else
+			pmo.health = cc["default"].health
+			pmo.maxhealth = pmo.health
+		end
 		
-		if (cc[pmo.skin].maxhealth) then pmo.maxhealth = cc[pmo.skin].maxhealth 
-		else pmo.maxhealth = cc["default"].maxhealth end
+		--if (cc[pmo.skin].maxhealth) then pmo.maxhealth = cc[pmo.skin].maxhealth 
+		--else pmo.maxhealth = cc["default"].maxhealth end
 	else
 		pmo.health = cc["default"].health
-		pmo.maxhealth = cc["default"].maxhealth 
+		pmo.maxhealth = pmo.health
 	end
 end
 
@@ -39,7 +47,7 @@ SRBZ.CharacterConfig = {
 		normalspeed = 19 * FRACUNIT,
 		sprintspeed = 23 * FRACUNIT,
 		health = 80,
-		maxhealth = 80,
+		--maxhealth = 80,
 		charability = CA_NONE,
 		charability2 = CA2_NONE,
 		jumpfactor = 17 * FRACUNIT / 19,
@@ -54,7 +62,7 @@ SRBZ.AddConfig("zzombie", {
 	normalspeed = 20 * FRACUNIT,
 	sprintspeed = 20 * FRACUNIT,
 	health = 90,
-	maxhealth = 90,
+	--maxhealth = 90,
 	charability = CA_NONE,
 	charability2 = CA2_NONE,
 	jumpfactor = 22 * FRACUNIT / 19,
@@ -65,7 +73,7 @@ SRBZ.AddConfig("sonic", {
 	normalspeed = 22 * FRACUNIT,
 	sprintspeed = 29 * FRACUNIT,
 	health = 80,
-	maxhealth = 80,
+	--maxhealth = 80,
 	charability = CA_JUMPTHOK,
 	charability2 = CA2_NONE,
 	jumpfactor = 17 * FRACUNIT / 19,
@@ -76,7 +84,7 @@ SRBZ.AddConfig("tails", {
 	normalspeed = 21 * FRACUNIT,
 	sprintspeed = 27 * FRACUNIT,
 	health = 95,
-	maxhealth = 95,
+	--maxhealth = 95,
 	charability = CA_FLY,
 	charability2 = CA2_NONE,
 	jumpfactor = 17 * FRACUNIT / 19,
@@ -87,7 +95,7 @@ SRBZ.AddConfig("knuckles", {
 	normalspeed = 17 * FRACUNIT,
 	sprintspeed = 24 * FRACUNIT,
 	health = 120,
-	maxhealth = 120,
+	--maxhealth = 120,
 	charability = CA_GLIDEANDCLIMB,
 	charability2 = CA2_NONE,
 	jumpfactor = 17 * FRACUNIT / 19,
@@ -98,7 +106,7 @@ SRBZ.AddConfig("amy", {
 	normalspeed = 20 * FRACUNIT,
 	sprintspeed = 27 * FRACUNIT,
 	health = 75,
-	maxhealth = 75,
+	--maxhealth = 75,
 	charability = CA_TWINSPIN,
 	charability2 = CA2_MELEE,
 	jumpfactor = 20 * FRACUNIT / 19,
@@ -108,7 +116,7 @@ SRBZ.AddConfig("fang", {
 	normalspeed = 18 * FRACUNIT,
 	sprintspeed = 24 * FRACUNIT,
 	health = 85,
-	maxhealth = 85,
+	--maxhealth = 85,
 	charability = CA_BOUNCE,
 	charability2 = CA2_GUNSLINGER,
 	jumpfactor = 20 * FRACUNIT / 19,
@@ -118,7 +126,7 @@ SRBZ.AddConfig("metalsonic", {
 	normalspeed = 17 * FRACUNIT,
 	sprintspeed = 24 * FRACUNIT,
 	health = 105,
-	maxhealth = 105,
+	--maxhealth = 105,
 	charability = CA_JUMPBOOST,
 	charability2 = CA2_NONE,
 	jumpfactor = 17 * FRACUNIT / 19,
