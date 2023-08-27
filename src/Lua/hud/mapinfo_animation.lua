66545a2aa6704344c51afd4c3094f24c4010a355
Jisk <37682565+Jiskster@users.noreply.github.com>
2023-08-26 21:10:26 -0500

    char select only
    oopsie
    
    levelname color change

--- src/Lua/hud/mapinfo_animation.lua
+++ src/Lua/hud/mapinfo_animation.lua
@@ -1,9 +1,8 @@
 SRBZ.mapinfohud = function(v, player)
-	
+	if not player.choosing then return end
+	if gametype ~= GT_SRBZ then return end
 	local mapinfo = mapheaderinfo[gamemap]
 	
-	if gametype ~= GT_SRBZ then return end
-	customhud.CustomFontString(v,0,46*FU, mapinfo.lvlttl:sub(1,SRBZ.TWRITE_MAPNAME_COUNT), "STCFC", V_SNAPTOLEFT|V_SNAPTOTOP, nil, FU, SKINCOLOR_WHITE)
+	customhud.CustomFontString(v,0,46*FU, mapinfo.lvlttl:sub(1,SRBZ.TWRITE_MAPNAME_COUNT), "STCFC", V_SNAPTOLEFT|V_SNAPTOTOP, nil, FU, SKINCOLOR_BLUE)
 	customhud.CustomFontString(v,0,54*FU, mapinfo.subttl:sub(1,SRBZ.TWRITE_COUNT), "STCFC", V_SNAPTOLEFT|V_SNAPTOTOP, nil, FU, SKINCOLOR_WHITE)
-
 end
