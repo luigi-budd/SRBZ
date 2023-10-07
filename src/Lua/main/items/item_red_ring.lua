0d6db30b147f0cffa1a231abf7b47ee33afb7879
Jisk <37682565+Jiskster@users.noreply.github.com>
2023-10-07 12:09:34 -0500

    tweak red ring

--- src/Lua/main/items/item_red_ring.lua
+++ src/Lua/main/items/item_red_ring.lua
@@ -4,11 +4,11 @@ SRBZ:CreateItem("Red Ring",  {
 	firerate = 17,
 	color = SKINCOLOR_RED,
 	knockback = 55*FRACUNIT,
-	damage = 15,
+	damage = 17,
 	onspawn = function(pmo, mo)
-		--1.5*FU = FU+FU/2 = (3*FU)/2 = 98304
-		mo.momx = FixedMul($,98304)
-		mo.momy = FixedMul($,98304)
-		mo.momz = FixedMul($,98304)
+		local mult = (4*FU)/2 
+		mo.momx = FixedMul($,mult)
+		mo.momy = FixedMul($,mult)
+		mo.momz = FixedMul($,mult)
 	end,
 })
